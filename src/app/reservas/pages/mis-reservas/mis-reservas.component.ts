2a7ae2531288de3c149021b37bf521d50694b1b9
Jahel <jahelcuadradomanchado@hotmail.com>
2023-01-13 21:53:34 +0100

    Creado el componente de mis reservas y añadido a las rutas

---
+++ src/app/reservas/pages/mis-reservas/mis-reservas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-reservas',
+  templateUrl: './mis-reservas.component.html',
+  styleUrls: ['./mis-reservas.component.css']
+})
+export class MisReservasComponent {
+
+}
